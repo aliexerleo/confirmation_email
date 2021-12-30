e247f75c88743decee93c90879f1ccf91d6b3a2e
aliexerleo <aliexermayor@hotmail.com>
2021-12-30 01:25:55 -0500

    modified method

--- apps/sendemail/views.py
+++ apps/sendemail/views.py
@@ -8,35 +8,21 @@ from django.core.mail import send_mail
 
 
 def index(request):
-    
-    return render(request, 'index.html', {})
+	
+	return render(request, 'index.html', {})
 
 class Registro(CreateView):
-        model = User
-        template_name = 'register.html'
-        form_class = RegistroForm 
-        def get_success_url(self):
-            return reverse('index') 
-            
-        send_mail('Subject here',
-                'Here is the message.',
-                'mayore@ebenezertechs.com',
-                ['mayoraliexer@gmail.com'],
-                fail_silently=False,
-            )
-        
-       
-def testing(request):
-    
-    return render(request, 'register.html', {})
-
-def send(request):
-    if request.method == 'POST':
-        mail = request.POST.get('mail')
-        send_mail('Delta', 
-        'Su registro ha sido exitoso', 
-        'mayore@ebenezertechs.com',
-        #['cc.puertadevida@gmail.com'],
-        [mail], 
-        fail_silently=False)
-    return render(request, 'result.html', {})
+		model = User
+		template_name = 'register.html'
+		form_class = RegistroForm 
+		def get_success_url(self):
+			return reverse('index') 
+			
+		send_mail(
+				'Subject here',
+				'Here is the message.',
+				'from@example.com',
+				['to@example.com'],
+				fail_silently=False,
+		)
+		
